c7d3d2b589422da7216d806163d7ea778858b09c
iamsainikhil <sainikhil.bheema@gmail.com>
2021-02-19 01:06:38 -0500

    setup and initialize GA to track key metrics

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,21 @@ import {ThemeProvider} from 'theme-ui'
 import theme from '../utils/theme'
 import {ToastContextProvider} from '../context/ToastContext'
 import Toast from './../components/Toast'
+import {initGA, trackGAEvent} from '../utils/googleAnalytics'
+
+// for more info on measuring app performance
+// visit https://nextjs.org/docs/advanced-features/measuring-performance
+export function reportWebVitals(metric) {
+  /* Google Analytics */
+  const {id, name, label, value} = metric
+  initGA()
+  trackGAEvent(
+    'Web Vitals',
+    'NextJS Custom Metric',
+    name,
+    Math.round(name === 'CLS' ? value * 1000 : value)
+  )
+}
 
 export default function MyApp({Component, pageProps}) {
   return (
