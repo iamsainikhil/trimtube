ae1b32cb145228393916c4e317a3aa05ce936180
iamsainikhil <sainikhil.bheema@gmail.com>
2021-02-27 17:53:52 -0500

    fix(head): replace hard-coded ga property with the env value

--- components/Head.jsx
+++ components/Head.jsx
@@ -341,35 +341,19 @@ const Head = ({
         {/* Global site tag (gtag.js) - Google Analytics  */}
         <script
           async
-          src='https://www.googletagmanager.com/gtag/js?id=G-RSV8ZGQNEY'></script>
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        />
         <script
           async
           defer
           dangerouslySetInnerHTML={{
             __html: `window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            
-              gtag('config', 'G-RSV8ZGQNEY');`,
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+              
+                gtag('config', ${process.env.NEXT_PUBLIC_GA_ID});`,
           }}
         />
-        {/* Hotjar Tracking Code */}
-        {isProd ? (
-          <script
-            async
-            defer
-            dangerouslySetInnerHTML={{
-              __html: `(function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:${hjid},hjsv:${hjsv}};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
-            }}
-          />
-        ) : null}
         {children}
       </NextHead>
     </>
