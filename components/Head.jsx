f811502f3dbce75e83ef45f615ae7f6350f1eb3c
Restyled.io <commits@restyled.io>
2021-02-27 23:14:50 +0000

    Restyled by whitespace

--- components/Head.jsx
+++ components/Head.jsx
@@ -352,7 +352,7 @@ const Head = ({
             __html: `window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-              
+
                 gtag('config', ${process.env.NEXT_PUBLIC_GA_ID});`,
           }}
         />
