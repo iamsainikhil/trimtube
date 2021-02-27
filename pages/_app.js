b35cb53fcd1ba83d5406237e3664e92809673413
Restyled.io <commits@restyled.io>
2021-02-27 23:14:45 +0000

    Restyled by clang-format

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,27 @@
 import '../styles/globals.scss'
 import '../styles/header.scss'
+
 import {ThemeProvider} from 'theme-ui'
-import theme from '../utils/theme'
+
 import {ToastContextProvider} from '../context/ToastContext'
-import Toast from './../components/Toast'
 import {trackGAEvent} from '../utils/googleAnalytics'
+import theme from '../utils/theme'
+
+import Toast from './../components/Toast'
 
 // for more info on measuring app performance
 // visit https://nextjs.org/docs/advanced-features/measuring-performance
 export function reportWebVitals(metric) {
   /* Google Analytics */
   const {id, name, label, value} = metric
-  trackGAEvent(
-    'Web Vitals',
-    'NextJS Custom Metric',
-    name,
-    Math.round(name === 'CLS' ? value * 1000 : value)
-  )
+  trackGAEvent('Web Vitals', 'NextJS Custom Metric', name,
+               Math.round(name === 'CLS' ? value * 1000 : value))
 }
 
 export default function MyApp({Component, pageProps}) {
-  return (
-    <ThemeProvider theme={theme}>
-      <ToastContextProvider>
-        <Component {...pageProps} />
+  return (<ThemeProvider theme = {theme}><ToastContextProvider>
+          <Component { ...pageProps } />
         <Toast />
-      </ToastContextProvider>
-    </ThemeProvider>
-  )
+          </ToastContextProvider>
+    </ThemeProvider>)
 }
--- utils/googleAnalytics.js
+++ utils/googleAnalytics.js
@@ -1,17 +1,17 @@
 import GA4React from 'ga-4-react'
 
 const ga4react = new GA4React(
-  process.env.NEXT_PUBLIC_GA_ID,
-  {
-    /* ga custom config, optional */
-  },
-  [
-    /* additional code, optional */
-  ],
-  5000 /* timeout, optional, defaults is 5000 */
+    process.env.NEXT_PUBLIC_GA_ID,
+    {
+        /* ga custom config, optional */
+    },
+    [
+      /* additional code, optional */
+    ],
+    5000 /* timeout, optional, defaults is 5000 */
 )
 
 export const trackGAEvent = (category, action, label, value = 0) => {
-  //ga4react.event(action, label, category)
+  // ga4react.event(action, label, category)
   ga4react.gtag('event', action, {category, label, value})
 }
