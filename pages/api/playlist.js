383a2c806688b8627694bf11a278d478e0d953e5
iamsainikhil <sainikhil.bheema@gmail.com>
2022-02-05 20:35:34 -0500

    refactor(Playlist): update api query param

--- pages/api/playlist.js
+++ pages/api/playlist.js
@@ -2,10 +2,10 @@ import axios from 'axios'
 import playlistData from '../../constants/playlistData'
 
 const playlist = async (req, res) => {
-  const {searchTerm} = req.query
+  const {id} = req.query
   try {
     const response = await axios.get(
-      `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails&maxResults=50&playlistId=${searchTerm}&key=${process.env.YOUTUBE_API_V3}`
+      `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails&maxResults=50&playlistId=${id}&key=${process.env.YOUTUBE_API_V3}`
     )
     res.status(200).json(response.data)
   } catch (error) {
