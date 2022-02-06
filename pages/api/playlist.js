a01cffa5bd1fc95118dfeadf4b4804254a840679
iamsainikhil <sainikhil.bheema@gmail.com>
2022-02-06 08:56:43 -0500

    fix(#18): update playlist api URL with pageToken

--- pages/api/playlist.js
+++ pages/api/playlist.js
@@ -2,10 +2,10 @@ import axios from 'axios'
 import playlistData from '../../constants/playlistData'
 
 const playlist = async (req, res) => {
-  const {id} = req.query
+  const {id, token} = req.query
   try {
     const response = await axios.get(
-      `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails&maxResults=50&playlistId=${id}&key=${process.env.YOUTUBE_API_V3}`
+      `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails&maxResults=50&playlistId=${id}&pageToken=${token}&key=${process.env.YOUTUBE_API_V3}`
     )
     res.status(200).json(response.data)
   } catch (error) {
