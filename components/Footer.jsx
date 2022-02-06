fffaf7fc7549446357ebe2a5901f1415cc5f0ffe
iamsainikhil <sainikhil.bheema@gmail.com>
2022-02-05 22:35:21 -0500

    fix footer and header background color

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,15 +6,14 @@ import {FaHeart} from 'react-icons/fa'
 import Icon from './Icon'
 import {trackGAEvent} from '../utils/googleAnalytics'
 import footerStyles from '../styles/footer.module.scss'
+import {Box, useColorModeValue} from '@chakra-ui/react'
 
 const Footer = () => {
   return (
-    <footer
-      sx={{
-        bg: 'muted',
-      }}>
-      <div className={footerStyles.footer}>
-        {/* <div className={footerStyles.linksRow}>
+    <footer>
+      <Box bg='mode.muted'>
+        <div className={footerStyles.footer}>
+          {/* <div className={footerStyles.linksRow}>
           <div>
             <a
               href="https://github.com/iamsainikhil/nextjs-prismic-blog-starter"
@@ -57,88 +56,89 @@ const Footer = () => {
           </div>
         </div> */}
 
-        <div
-          style={{
-            textAlign: 'center',
-            margin: '0.25rem auto',
-            wordSpacing: '0.2rem',
-          }}>
-          Made with{' '}
-          <FaHeart
-            style={{color: '#CC3D5C', marginBottom: '-0.25rem'}}
-            aria-label='Love'
-            title='Love'
-          />{' '}
-          using{' '}
-          <SiNextdotjs
-            sx={{
-              color: 'text',
-              marginBottom: '-0.25rem',
-              cursor: 'help',
-            }}
-            aria-label='NextJS'
-            title='NextJS'
-          />
+          <div
+            style={{
+              textAlign: 'center',
+              margin: '0.25rem auto',
+              wordSpacing: '0.2rem',
+            }}>
+            Made with{' '}
+            <FaHeart
+              style={{color: '#CC3D5C', marginBottom: '-0.25rem'}}
+              aria-label='Love'
+              title='Love'
+            />{' '}
+            using{' '}
+            <SiNextdotjs
+              sx={{
+                color: 'text',
+                marginBottom: '-0.25rem',
+                cursor: 'help',
+              }}
+              aria-label='NextJS'
+              title='NextJS'
+            />
+          </div>
+
+          <div className={footerStyles.socialRow}>
+            <Icon
+              name='Twitter'
+              url='https://twitter.com/iamsainikhil12'
+              style={{
+                color: 'primary',
+                fontSize: '1.5rem',
+              }}
+            />
+
+            <Icon
+              name='GitHub'
+              url='https://github.com/iamsainikhil'
+              style={{
+                color: 'primary',
+                fontSize: '1.5rem',
+              }}
+            />
+
+            <Icon
+              name='LinkedIn'
+              url='https://www.linkedin.com/in/iamsainikhil'
+              style={{
+                color: 'primary',
+                fontSize: '1.70rem',
+                marginBottom: '0.25rem',
+              }}
+            />
+          </div>
+
+          <p
+            style={{
+              display: 'flex',
+              flexFlow: 'row nowrap',
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: '0.5rem',
+            }}>
+            © {new Date().getFullYear()}{' '}
+            <a
+              href='https://iamsainikhil.com'
+              target='_blank'
+              rel='noreferrer noopener'
+              className='special-link'
+              aria-label='Portfolio'
+              title='Portfolio'
+              sx={{color: 'primary', marginLeft: '0.25rem'}}
+              onClick={() =>
+                trackGAEvent(
+                  'footer links',
+                  `clicked on copyright link in Footer`,
+                  'text click'
+                )
+              }>
+              Sai&nbsp;Nikhil
+            </a>
+          </p>
         </div>
-
-        <div className={footerStyles.socialRow}>
-          <Icon
-            name='Twitter'
-            url='https://twitter.com/iamsainikhil12'
-            style={{
-              color: 'primary',
-              fontSize: '1.5rem',
-            }}
-          />
-
-          <Icon
-            name='GitHub'
-            url='https://github.com/iamsainikhil'
-            style={{
-              color: 'primary',
-              fontSize: '1.5rem',
-            }}
-          />
-
-          <Icon
-            name='LinkedIn'
-            url='https://www.linkedin.com/in/iamsainikhil'
-            style={{
-              color: 'primary',
-              fontSize: '1.70rem',
-              marginBottom: '0.25rem',
-            }}
-          />
-        </div>
-
-        <p
-          style={{
-            display: 'flex',
-            flexFlow: 'row nowrap',
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginTop: '0.5rem',
-          }}>
-          © {new Date().getFullYear()}{' '}
-          <a
-            href='https://iamsainikhil.com'
-            target='_blank'
-            rel='noreferrer noopener'
-            className='special-link'
-            aria-label='Portfolio'
-            title='Portfolio'
-            sx={{color: 'primary', marginLeft: '0.25rem'}}
-            onClick={() =>
-              trackGAEvent(
-                'footer links',
-                `clicked on copyright link in Footer`,
-                'text click'
-              )
-            }>
-            Sai&nbsp;Nikhil
-          </a>
-        </p>
-      </div>
+      </Box>
     </footer>
   )
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,7 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import {jsx, useThemeUI} from 'theme-ui'
+import {useColorMode, Box} from '@chakra-ui/react'
 import {default as NextLink} from 'next/link'
 import Headroom from 'react-headroom'
 import {GoSearch} from 'react-icons/go'
@@ -11,7 +12,8 @@ import {trackGAEvent} from '../utils/googleAnalytics'
 import {useRouter} from 'next/router'
 
 const Header = () => {
-  const {colorMode, setColorMode} = useThemeUI()
+  // const {colorMode, setColorMode} = useThemeUI()
+  const {colorMode, toggleColorMode} = useColorMode()
   const router = useRouter()
   const {asPath: routePath} = router
   const iconStyle = {
@@ -27,124 +29,126 @@ const Header = () => {
 
   return (
     <Headroom disableInlineStyles upTolerance={10} downTolerance={10}>
-      <header sx={{bg: 'muted'}} className='header'>
-        <div className='header-content'>
-          <div
-            sx={{
-              m: 0,
-              variant: 'textStyles.title',
-              fontSize: ['2rem', 5, 6],
-            }}>
-            <NextLink href='/' passHref>
-              <a
-                sx={{
-                  variant: 'styles.a',
-                  textDecoration: 'none',
-                  fontFamily: `'Damion', 'Lato', -apple-system, BlinkMacSystemFont, Segoe UI, sans-serif`,
-                  letterSpacing: '0.15rem',
-                }}
-                onClick={() =>
-                  trackGAEvent('logo', `clicked on site logo`, 'link click')
-                }
-                rel='noreferrer noopener'>
-                TrimTube
-              </a>
-            </NextLink>
-          </div>
-          <div className='header-links'>
-            {routePath !== '/' ? (
-              <p>
-                <GoSearch
-                  title='Search'
+      <Box bg='mode.muted'>
+        <header className='header'>
+          <div className='header-content'>
+            <div
+              sx={{
+                m: 0,
+                variant: 'textStyles.title',
+                fontSize: ['2rem', 5, 6],
+              }}>
+              <NextLink href='/' passHref>
+                <a
                   sx={{
-                    fontSize: '1.75rem',
-                    ...iconStyle,
-                    marginTop: '1rem',
+                    variant: 'styles.a',
+                    textDecoration: 'none',
+                    fontFamily: `'Damion', 'Lato', -apple-system, BlinkMacSystemFont, Segoe UI, sans-serif`,
+                    letterSpacing: '0.15rem',
                   }}
-                  onClick={() => {
-                    trackGAEvent(
-                      'search',
-                      `clicked on search icon`,
-                      'icon click'
-                    )
-                    router.push('/')
-                  }}
-                />
-              </p>
-            ) : null}
-            {routePath !== '/playlists' ? (
+                  onClick={() =>
+                    trackGAEvent('logo', `clicked on site logo`, 'link click')
+                  }
+                  rel='noreferrer noopener'>
+                  TrimTube
+                </a>
+              </NextLink>
+            </div>
+            <div className='header-links'>
+              {routePath !== '/' ? (
+                <p>
+                  <GoSearch
+                    title='Search'
+                    sx={{
+                      fontSize: '1.75rem',
+                      ...iconStyle,
+                      marginTop: '1rem',
+                    }}
+                    onClick={() => {
+                      trackGAEvent(
+                        'search',
+                        `clicked on search icon`,
+                        'icon click'
+                      )
+                      router.push('/')
+                    }}
+                  />
+                </p>
+              ) : null}
+              {routePath !== '/playlists' ? (
+                <p>
+                  <NextLink href={'/playlists'} passHref>
+                    <a>
+                      <RiPlayListFill
+                        title='Playlists'
+                        sx={{
+                          fontSize: '1.85rem',
+                          ...iconStyle,
+                        }}
+                      />
+                    </a>
+                  </NextLink>
+                </p>
+              ) : null}
               <p>
-                <NextLink href={'/playlists'} passHref>
-                  <a>
-                    <RiPlayListFill
-                      title='Playlists'
+                <a
+                  href='https://github.com/iamsainikhil/trimtube#--trimtube'
+                  target='_blank'
+                  rel='noopener noreferrer'>
+                  <FiHelpCircle
+                    title='About'
+                    sx={{
+                      fontSize: '2rem',
+                      ...iconStyle,
+                    }}
+                  />
+                </a>
+              </p>
+              <p>
+                {colorMode !== 'dark' ? (
+                  <span
+                    title='Switch to Dark Mode'
+                    aria-label='Switch to Dark Mode'>
+                    <FiSun
                       sx={{
-                        fontSize: '1.85rem',
+                        fontSize: '2rem',
                         ...iconStyle,
                       }}
+                      onClick={() => {
+                        toggleColorMode()
+                        trackGAEvent(
+                          'toggle theme',
+                          `enabled dark theme`,
+                          'icon click'
+                        )
+                      }}
                     />
-                  </a>
-                </NextLink>
+                  </span>
+                ) : (
+                  <span
+                    title='Switch to Light Mode'
+                    aria-label='Switch to Light Mode'>
+                    <FiMoon
+                      sx={{
+                        fontSize: '2rem',
+                        ...iconStyle,
+                      }}
+                      onClick={() => {
+                        toggleColorMode()
+                        trackGAEvent(
+                          'toggle theme',
+                          `enabled light theme`,
+                          'icon click'
+                        )
+                      }}
+                    />
+                  </span>
+                )}
               </p>
-            ) : null}
-            <p>
-              <a
-                href='https://github.com/iamsainikhil/trimtube#--trimtube'
-                target='_blank'
-                rel='noopener noreferrer'>
-                <FiHelpCircle
-                  title='About'
-                  sx={{
-                    fontSize: '2rem',
-                    ...iconStyle,
-                  }}
-                />
-              </a>
-            </p>
-            <p>
-              {colorMode !== 'dark' ? (
-                <span
-                  title='Switch to Dark Mode'
-                  aria-label='Switch to Dark Mode'>
-                  <FiSun
-                    sx={{
-                      fontSize: '2rem',
-                      ...iconStyle,
-                    }}
-                    onClick={() => {
-                      setColorMode('dark')
-                      trackGAEvent(
-                        'toggle theme',
-                        `enabled dark theme`,
-                        'icon click'
-                      )
-                    }}
-                  />
-                </span>
-              ) : (
-                <span
-                  title='Switch to Light Mode'
-                  aria-label='Switch to Light Mode'>
-                  <FiMoon
-                    sx={{
-                      fontSize: '2rem',
-                      ...iconStyle,
-                    }}
-                    onClick={() => {
-                      setColorMode('light')
-                      trackGAEvent(
-                        'toggle theme',
-                        `enabled light theme`,
-                        'icon click'
-                      )
-                    }}
-                  />
-                </span>
-              )}
-            </p>
+            </div>
           </div>
-        </div>
-      </header>
+        </header>
+      </Box>
     </Headroom>
   )
 }
