51fc12589e7412670e07044090dd855c85e4edde
iamsainikhil <sainikhil.bheema@gmail.com>
2022-02-13 02:50:15 -0500

    fix(Head): update site description

--- components/Head.jsx
+++ components/Head.jsx
@@ -14,7 +14,7 @@ const Head = ({
   const pageTitle = `${title ? `${title} | ` : ''}${page} | ${siteName}`
   const metaDescription =
     description ||
-    'TrimTube is a web application which allows the user to search for YouTube videos or paste any YouTube video link. This app also features a media player that allows the user to trim and loop any portion of a YouTube video with ability to save the video(s) to a playlist.'
+    'TrimTube is a web application which allows user to fetch video or a playlist using a YouTube video or playlist link. This app also features a media player that allows the user to trim and loop any portion of a YouTube video with ability to save the video(s) to a playlist(s).'
   const isProd = process.env.NODE_ENV === 'production'
   // Hotjar variables
   const hjid = process.env.NEXT_PUBLIC_HOTJAR_ID
