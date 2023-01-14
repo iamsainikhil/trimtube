dcd9c27f80577a43d2de77af7a6dffcb3df2622f
iamsainikhil <sainikhil.bheema@gmail.com>
2023-01-14 15:25:18 -0500

    fix(PlaylistVideos): set autoscroll block to nearest

--- components/PlaylistVideos.jsx
+++ components/PlaylistVideos.jsx
@@ -270,9 +270,8 @@ const PlaylistVideos = ({
   useEffect(() => {
     if (refs[videoNumber - 1] && refs[videoNumber - 1].current) {
       refs[videoNumber - 1].current.scrollIntoView({
-        top: 100,
         behavior: 'smooth',
-        block: 'end',
+        block: 'nearest',
       })
     }
     return () => {}
