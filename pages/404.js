db5d4cc36016df68dad065563a39f0ecd889b2dd
iamsainikhil <sainikhil.bheema@gmail.com>
2026-06-02 19:23:32 -0400

    fix: load lottie dynamically

--- pages/404.js
+++ pages/404.js
@@ -2,11 +2,13 @@
 /** @jsx jsx */
 import {jsx} from 'theme-ui'
 import dynamic from 'next/dynamic'
-import Lottie from 'react-lottie'
 import Loader from '../components/Loader'
 import animationData from '../public/404_animation.json'
 const Layout = dynamic(() => import('../components/Layout'), {
   loading: Loader,
+})
+const Lottie = dynamic(() => import('react-lottie'), {
+  ssr: false,
 })
 
 export default function Custom404() {
